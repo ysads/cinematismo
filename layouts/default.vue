0ee1930c9e214c9f9566324fb7d06b53f382ecbd
Ygor Sad <ysads2010@gmail.com>
2020-05-03 20:37:12 -0300

    refactor(layouts/default): remove own style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,68 +1,17 @@
 <template>
   <div>
-    <the-header />
     <the-nav-bar />
     <nuxt />
   </div>
 </template>
 
 <script>
-import TheHeader from '~/components/the_header'
 import TheNavBar from '~/components/the_nav_bar'
 
 export default {
   name: 'default',
   components: {
-    TheHeader,
     TheNavBar
   }
 }
 </script>
-
-<style>
-html
-{
-  font-family: "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*, *:before, *:after
-{
-  box-sizing: border-box;
-  margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
