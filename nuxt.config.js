a691a0720939f6adaebe5c50c9b59662b82f18e7
Ygor Sad <ysads2010@gmail.com>
2020-05-15 00:33:44 -0300

    feat(assets/*): improve reset and add new colors and fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css' }
     ],
